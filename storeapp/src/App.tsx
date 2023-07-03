406347e08c8345ee7e20c91e77c4daa210d91556
Ghufran <ghufranshahid101@gmail.com>
2023-07-03 18:55:19 -0400

    Added menu bar and bg video

--- storeapp/src/App.tsx
+++ storeapp/src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './stylesheets/App.scss';
-
+import bgVideo from "./assets/cityScape.mp4"
+import { AspectRatio } from '@radix-ui/react-aspect-ratio';
 function App() {
 
   return (
-    <div className="flex h-[100vh]">
-        Hello World
-    </div>
+    <>
+
+      <div className='flex bg-slate-100 p-6'>Tool bar</div>
+
+      <div className="">
+          <video src={bgVideo} autoPlay loop muted className="min-w-full p-0"></video>
+      </div>
+
+    </>
   );
 }
 
