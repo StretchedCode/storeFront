e81c453b74f6d981b03d024b782a162e45bee057
Ghufran <ghufranshahid101@gmail.com>
2023-07-04 19:02:52 -0400

    Added function to process cpu JSON data

--- storeapp/src/database/db.ts
+++ storeapp/src/database/db.ts
@@ -2,16 +2,40 @@
 
 import cpuData from "./tableExportCPU.json"
 import gpuData from "./tableExportGPU.json"
+import cpuImage from "../assets/processorImg.jpeg"
 
 // Mock database file for loading shop components
 
-//const processorDB: {title: string, price: number, imgSrc: string,  }[] = [
-//    {
+interface product{
+    type: 'cpu' | 'gpu';
+    name: string,
+    price: number,
+    id: number,
+    imgSrc?: string
+}
 
-//    }
-//]
+const processCPUData = () => {
+    const processorData: product[] = []
+
+    for(let i = 1; i <= 21; i++){
+        const dataObj:product = {
+            type: 'cpu',
+            name: (cpuData.data[i].Model).toString(),
+            price: 500,
+            id: i,
+            imgSrc: cpuImage
+        }
+        processorData.push(dataObj)
+    }
+
+    return processorData
+}
+
 
 export default function seeData(){
     console.log(cpuData)
     console.log(gpuData)
 }
+
+export {processCPUData}
+export type {product as productType}
