30e37d9ea050b6e0d6b4916389846ed9522d289a
Ghufran <ghufranshahid101@gmail.com>
2023-07-03 21:16:07 -0400

    Added navMenu component

--- storeapp/src/components/homebar.tsx
+++ storeapp/src/components/homebar.tsx
@@ -1,5 +1,6 @@
 import { BackpackIcon } from "@radix-ui/react-icons"
 import { Button } from "./button"
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "./navigation-menu"
 
 
 
@@ -8,6 +9,20 @@ function Taskbar(){
         <div className="flex justify-between items-center p-6">
             <div className="text-xl">Home Bar</div>
             <div className="flex gap-3">
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>
+                                Shop
+                            </NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <NavigationMenuLink>
+                                    Placeholder one
+                                </NavigationMenuLink>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
                 <Button variant="outline">Sign In</Button>
                 <Button variant="outline" className="bg-white" size="icon"><BackpackIcon color="black"></BackpackIcon></Button>
             </div>
