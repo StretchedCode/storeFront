964cc5930720e466944450119b506c5a8d0d94e9
Ghufran <ghufranshahid101@gmail.com>
2023-07-03 20:34:37 -0400

    Imported and referenced Taskbar component

--- storeapp/src/App.tsx
+++ storeapp/src/App.tsx
@@ -2,25 +2,27 @@ import React from 'react';
 import './stylesheets/App.scss';
 import bgImage from './assets/cnTowerImage.jpg'
 import { AspectRatio } from '@radix-ui/react-aspect-ratio';
+import { Button } from './components/button';
+import Taskbar from './components/homebar';
 function App() {
 
   return (
     <div>
       <div className="flex flex-col">
-        <div className="flex">
 
-        </div>
+        <Taskbar></Taskbar>
         
         <div className="bg-[url('../assets/cnTowerImage.jpg')]">
 
           <div className="grid grid-cols-3 h-screen">
 
-            <div className="flex justify-center items-center bg-black opacity-70">
+            <div className="flex justify-center items-center bg-black opacity-80 flex-col gap-10">
 
               <div className="opacity-100 text-white font-extrabold text-7xl px-5">
                 Life is short, so isn't it worth living to the fullest?
-
               </div>
+
+              <Button variant="outline" className="bg-black text-white font-bold rounded-none w-60">Explore Life</Button>
 
             </div>
 
@@ -29,7 +31,7 @@ function App() {
         </div>
 
       </div>
-      
+
     </div>
   );
 }
