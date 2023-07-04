bd681eedfbcf62258b2fafd5ba1f9ea4bb9696a3
Ghufran <ghufranshahid101@gmail.com>
2023-07-04 16:06:44 -0400

    Added grid styling

--- storeapp/src/pages/shop.tsx
+++ storeapp/src/pages/shop.tsx
@@ -3,7 +3,21 @@
 function Shop(){
 
     return(
-        <div>Shopping Page</div>
+        <div className="grid grid-cols-5 min-h-[90vh]">
+            <div className="cols-span-1">
+                Col 1
+            </div>
+
+            <div className="flex flex-col col-span-4">
+                <div className="shadow-md">
+                    Top of Col 2
+                </div>
+
+                <div className="bg-slate-50">Col 2</div>
+
+            </div>
+
+        </div>
     )
 }
 
