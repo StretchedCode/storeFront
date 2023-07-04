84e3ad015b5f0470f77f2ad5c01fa4675a9bf084
Ghufran <ghufranshahid101@gmail.com>
2023-07-04 15:28:41 -0400

    Added mock database, alongside JSON files for information

---
+++ storeapp/src/database/db.ts
@@ -0,0 +1,17 @@
+
+
+import cpuData from "./tableExportCPU.json"
+import gpuData from "./tableExportGPU.json"
+
+// Mock database file for loading shop components
+
+//const processorDB: {title: string, price: number, imgSrc: string,  }[] = [
+//    {
+
+//    }
+//]
+
+export default function seeData(){
+    console.log(cpuData)
+    console.log(gpuData)
+}
