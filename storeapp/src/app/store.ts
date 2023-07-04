736fbe74ec8a120782707cb837916b96eae344a0
Ghufran <ghufranshahid101@gmail.com>
2023-07-04 19:05:01 -0400

    Removed pokemon reducer from store

--- storeapp/src/app/store.ts
+++ storeapp/src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import pokemonReducer from '../features/pokemonSlice';
 import shopReducer from "../features/shopSlice"
 
 export const store = configureStore({
   reducer: {
-    pokemon: pokemonReducer,
     shop: shopReducer
   },
 });
