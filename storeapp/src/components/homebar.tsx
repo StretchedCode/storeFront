7f80b8cea649e6a89440e3286fabb032c7d3c774
Ghufran <ghufranshahid101@gmail.com>
2023-07-05 11:21:59 -0400

    Manually added gpu portion of Navlink; added ShoppingCart component into component structure

--- storeapp/src/components/homebar.tsx
+++ storeapp/src/components/homebar.tsx
@@ -3,16 +3,21 @@ import { Button } from "./button"
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "./navigation-menu"
 import { useNavigate, Link } from "react-router-dom"
 import cpuImage from "../assets/cpu.png"
+import gpuImage from "../assets/gpu-mining.png"
+import { useAppSelector } from "../app/hooks"
+import Badge from "./badge"
+import ShoppingCart from "./shoppingCart"
 
 
 
-function Taskbar(){
+function Homebar(){
 
     const navigate = useNavigate()
+    const itemCount = useAppSelector(state => state.shop.totalProducts)
 
     return (
-        <div className="flex justify-between items-center p-6 shadow-sm">
-            <Button variant="ghost" className="text-xl hover:bg-white" onClick={() => {navigate("/")}}>Home Bar</Button>
+        <div className="flex justify-between items-center p-8 shadow-sm">
+            <Button variant="ghost" className="text-xl hover:bg-white" onClick={() => {navigate("/")}}>Home</Button>
             <div className="flex gap-3">
                 <NavigationMenu className="mr-48" orientation="horizontal">
                     <NavigationMenuList>
@@ -22,15 +27,32 @@ function Taskbar(){
                             </NavigationMenuTrigger>
                             <NavigationMenuContent className="">
                                 <div className="grid grid-cols-1 w-[400px] gap-2 p-2">
-                                    <NavigationMenuLink href="/shop/processors" className="flex gap-2 gap-x-4 p-2 hover:bg-slate-100 rounded-md">
-                                        <div className="self-center">
-                                            <img className="h-16 min-w-[64px]" alt="processor" src={cpuImage}></img>
-                                        </div>
-                                        
-                                        <div className="flex flex-col gap-2">
-                                            <div className="text-m font-semibold">Processors</div>
-                                            <div className="text-sm">The backbone of any computer, processors manage all of the components and data that passes through your computer.</div>
-                                        </div>
+                                    <NavigationMenuLink asChild>
+                                        <Link to="/shop/processors" state={{type: "cpu"}} className="flex gap-2 gap-x-4 p-2 hover:bg-slate-100 rounded-md">
+                                            <div className="self-center">
+                                                <img className="h-16 min-w-[64px]" alt="processor" src={cpuImage}></img>
+                                            </div>
+                                            
+                                            <div className="flex flex-col gap-2">
+                                                <div className="text-m font-semibold">Processors</div>
+                                                <div className="text-sm">The backbone of any computer, processors manage all of the components and data that passes through your computer.</div>
+                                            </div>
+                                        </Link>
+                                    </NavigationMenuLink>
+                                    <NavigationMenuLink asChild>
+                                        <Link to="/shop/gpus" state={{type: "gpu"}} className="flex gap-2 gap-x-4 p-2 hover:bg-slate-100 rounded-md">
+                                            <div className="self-center">
+                                                <img className="h-16 min-w-[64px]" alt="processor" src={gpuImage}></img>
+                                            </div>
+                                            
+                                            <div className="flex flex-col gap-2">
+                                                <div className="text-m font-semibold">Graphics Cards</div>
+                                                <div className="text-sm">
+                                                    GPUs are the main component in rendering any sort of visual interface for the user. Large processing units
+                                                    used in applications such as gaming, rendering and multi-media applications.
+                                                </div>
+                                            </div>
+                                        </Link>
                                     </NavigationMenuLink>
                                 </div>
                             </NavigationMenuContent>
@@ -38,10 +60,10 @@ function Taskbar(){
                     </NavigationMenuList>
                 </NavigationMenu>
                 <Button variant="outline">Sign In</Button>
-                <Button variant="outline" className="bg-white" size="icon"><BackpackIcon color="black"></BackpackIcon></Button>
+                <ShoppingCart></ShoppingCart>
             </div>
         </div>
     )
 }
 
-export default Taskbar
+export default Homebar
