9126015a4297cb4dcc8c62904354d2198e4fd3b7
Ghufran <ghufranshahid101@gmail.com>
2023-07-05 11:27:04 -0400

    Added filter function to conditionally render data based on user path through routes, added routes for product types

--- storeapp/src/pages/shop.tsx
+++ storeapp/src/pages/shop.tsx
@@ -1,30 +1,44 @@
-import { processCPUData } from "../database/db"
+import { processData, productType } from "../database/db"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../components/card"
-import { Skeleton } from "../components/skeleton"
-import { render } from "@testing-library/react"
 import { Button } from "../components/button"
 import { Separator } from "../components/separator"
 import { useAppDispatch } from "../app/hooks"
 import { addProduct } from "../features/shopSlice"
 import { Routes, Route } from "react-router-dom"
+import { useLocation } from "react-router-dom"
 
 function Shop(){
     const dispatch = useAppDispatch()
-    const data = processCPUData()
+    const data = processData()
+    const linkState = useLocation().state
+    const linkData = linkState === null ? "all" : linkState.type
 
-    const renderCPUData = () => {
+    const filterCheck = (obj: productType, filterType: string) => {
+        console.log('called')
+        if (filterType === "all"){
+            return true
+        } else if (obj.type !== filterType){
+            return false
+        } else {
+            return true
+        }
+    }
+
+    const renderData = (filterType: 'cpu' | 'gpu' | 'all') => {
         const dataVis: React.ReactElement[] = []
-
-        data.forEach((obj) => {
+        console.log(filterType)
+        const filteredData = data.filter((obj) => filterCheck(obj, filterType))
+        console.log(filteredData)
+        filteredData.forEach((obj) => {
             const dataCard = <>
-            <Card className="flex flex-col justify-center">
+            <Card className="flex flex-col justify-center transition ease-in-out delay-50 hover:-translate-y-4 duration-150">
                 <CardHeader className="flex justify-center">
                     <CardTitle className="">{obj.name}</CardTitle>
                     <CardDescription>{obj.type}</CardDescription>
                     <Separator></Separator>
                 </CardHeader>
                 <CardContent className="flex flex-col gap-6 justify-center">
-                    <img src={obj.imgSrc}></img>
+                    <img src={obj.imgSrc} alt={obj.name}></img>
                     <Separator></Separator>
                     <div className="flex justify-between">
                         <div>${obj.price}</div>
@@ -42,15 +56,17 @@ function Shop(){
     return(
         <div className="grid grid-cols-5 min-h-[90vh]">
             <div className="cols-span-1">
-                Col 1
+                
             </div>
 
             <div className="flex flex-col col-span-4">
                 <div className="shadow-sm py-4"></div>
 
-                <div className="bg-slate-50 grid grid-cols-3 gap-5 p-5">
+                <div className="bg-slate-50 grid grid-cols-3 gap-5 p-5 animate-in">
                     <Routes>
-                        <Route path="/processors" element={renderCPUData()}></Route>
+                        <Route index element={renderData(linkData)}></Route>
+                        <Route path="/processors" element={renderData(linkData)}></Route>
+                        <Route path="/gpus" element={renderData(linkData)}></Route>
                     </Routes>
                 </div>
 
